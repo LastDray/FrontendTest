131f88ff41702f0e08ed38699cf9da343c74a0d8
LastDray <kidner781@yandex.ru>
2023-06-10 13:51:09 +0300

    First commit

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log(4 + '5');
+
+
+let incr = 10;
+let decr = 10;
+
+let x = incr++;
+//--decr;
+
+console.log(`x = ${x}`)
+console.log(`incr = ${incr}`)
+
+console.log(2 + 6 != "8");
+
+const isChecked = true,
+      isClose = false;
+
+console.log(!isChecked || isClose);
+
